4c863ff95863db3ea82f3aacb7eec381169bae73
skipancho <ctg1770a@gmail.com>
2021-12-23 18:29:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/smartshopping2/App.kt
@@ -0,0 +1,16 @@
+package com.example.smartshopping2
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+    
+    companion object{
+        lateinit var instance : App
+        const val API_HOST = "10.0.0.2"
+        const val API_PORT = 8080
+    }
+}
