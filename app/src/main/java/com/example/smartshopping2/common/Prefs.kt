00a8054cb9cc5315553762523a60af1883097f69
skipancho <ctg1770a@gmail.com>
2022-01-02 15:50:37 +0900

    + 장바구니 목록 저장

--- app/src/main/java/com/example/smartshopping2/common/Prefs.kt
+++ app/src/main/java/com/example/smartshopping2/common/Prefs.kt
@@ -1,7 +1,13 @@
 package com.example.smartshopping2.common
 
+import android.util.Log
 import androidx.preference.PreferenceManager
 import com.example.smartshopping2.App
+import com.example.smartshopping2.api.response.ProductResponse
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
+import org.json.JSONArray
 
 object Prefs {
     private const val TOKEN = "token"
@@ -11,6 +17,8 @@ object Prefs {
 
     private const val USER_ID = "user_id"
     private const val USER_PW = "user_pw"
+
+    private const val CART_LIST = "cart_list"
 
     val prefs by lazy {
         PreferenceManager.getDefaultSharedPreferences(App.instance)
@@ -39,5 +47,37 @@ object Prefs {
     var password
         get() = prefs.getString(USER_PW, null)
         set(value) = prefs.edit().putString(USER_PW,value).apply()
+
+    var cartList: List<ProductResponse>
+        get() {
+            val json = prefs.getString(CART_LIST, null)
+            val gson = GsonBuilder().create()
+            val list = ArrayList<ProductResponse>()
+
+            try {
+                json?.let {
+                    val jsonArray = JSONArray(it)
+                    for(i in 0 until jsonArray.length()){
+                        val product = gson.fromJson(
+                            jsonArray.get(i).toString(),
+                            ProductResponse::class.java)
+                        list.add(product)
+                    }
+                }
+            }catch (e : Exception){
+                Log.e("Prefs","cartList get() error")
+            }
+
+            return list
+        }
+        set(value) {
+            val gson = GsonBuilder().create()
+            val jsonArray = JSONArray()
+            for (product in value){
+                val json = gson.toJson(product, ProductResponse::class.java)
+                jsonArray.put(json)
+            }
+            prefs.edit().putString(CART_LIST,jsonArray.toString()).apply()
+        }
 
 }
