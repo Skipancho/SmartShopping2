abc7cb92583ae0c2d1a8ffd0cf71bb2bbe1a3b75
skipancho <ctg1770a@gmail.com>
2022-01-02 21:53:30 +0900

    + priceText 프로퍼티 추가

--- app/src/main/java/com/example/smartshopping2/api/response/ProductResponse.kt
+++ app/src/main/java/com/example/smartshopping2/api/response/ProductResponse.kt
@@ -1,6 +1,9 @@
 package com.example.smartshopping2.api.response
 
-data class ProductResponse(
+import com.example.smartshopping2.domain.product.ProductStatus
+import java.text.NumberFormat
+
+class ProductResponse(
     val id : Long,
     val name : String,
     val description : String,
@@ -8,4 +11,20 @@ data class ProductResponse(
     val status : String,
     val sellerId : Long,
     val imagePaths : List<String>
-)
+){
+    val priceText : String
+        get() {
+            val soldOutString =
+                if (ProductStatus.SOLD_OUT == status)
+                    "(품절)"
+                else ""
+            val commaSeparatedPrice =
+                NumberFormat.getNumberInstance().format(price)
+            return "₩$commaSeparatedPrice $soldOutString"
+        }
+
+    override fun toString(): String {
+        return " id : $id, name : $name, description : $description, " +
+                "price : $priceText, status : $status"
+    }
+}
